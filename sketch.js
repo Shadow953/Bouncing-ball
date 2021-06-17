46040a04fabf75334ee9c6d7ab48ac253322c489
Shadow953 <85889404+Shadow953@users.noreply.github.com>
2021-06-17 14:44:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+//namespacing- another name
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+var ground;
+var angle=60;
+var poly;
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  //options object {} :(=) ,(;)
+   var ball_options = {
+
+     //changing the properties of body
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+//displaying shape at the position of ball body
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,400,20);
+ 
+
+
+  
+  
+}
+
